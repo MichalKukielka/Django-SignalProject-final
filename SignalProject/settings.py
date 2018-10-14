02c63ef26d868006bee159e9cb706f02af31753e
MichalKukielka <kukielka.michal@gmail.com>
2018-10-14 21:34:19 +0200

    Update SignalProject/settings.py

--- SignalProject/settings.py
+++ SignalProject/settings.py
@@ -97,6 +97,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
