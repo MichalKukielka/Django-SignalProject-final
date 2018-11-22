b3049b07862922021490c0d92e282e318344e734
Michał Kukiełka <kukielka.michal@gmail.com>
2018-11-22 16:38:37 +0100

    Update settings.py

--- SignalProject/settings.py
+++ SignalProject/settings.py
@@ -63,8 +63,12 @@ WSGI_APPLICATION = 'SignalProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'SignalProjectdb',
+        'USER': 'superuser',
+        'PASSWORD': 'superpass',
+        'HOST':'127.0.0.1',
+        'PORT': '5432',
     }
 }
 
